2f03275d7195a4e88f48ec8e0b42ca3c8dd689e3
guidolarosa <guido.glarosa@gmail.com>
2023-03-29 18:12:57 -0300

    Create message ct

--- config/plugins.js
+++ config/plugins.js
@@ -14,17 +14,17 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  email: {
-    config: {
-      provider: 'sendgrid', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
-      providerOptions: {
-        apiKey: env('SENDGRID_API_KEY'),
-      },
-      settings: {
-        defaultFrom: 'hudsonriver.adm@gmail.com',
-        defaultReplyTo: 'hudsonriver.adm@gmail.com',
-        testAddress: 'hudsonriver.adm@gmail.com',
-      },
-    },
-  },
+  // email: {
+  //   config: {
+  //     provider: 'sendgrid', // For community providers pass the full package name (e.g. provider: 'strapi-provider-email-mandrill')
+  //     providerOptions: {
+  //       apiKey: env('SENDGRID_API_KEY'),
+  //     },
+  //     settings: {
+  //       defaultFrom: 'hudsonriver.adm@gmail.com',
+  //       defaultReplyTo: 'hudsonriver.adm@gmail.com',
+  //       testAddress: 'hudsonriver.adm@gmail.com',
+  //     },
+  //   },
+  // },
 });
--- src/api/email/routes/email.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  routes: [
-    {
-      method: 'POST',
-      path: '/email',
-      handler: 'email.sendContactEmail',
-    }
-  ]
-}
