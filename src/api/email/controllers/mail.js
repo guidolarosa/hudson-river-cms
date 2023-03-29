b9af027e7ae0bf7852e3e65294c845ee78d4961e
guidolarosa <guido.glarosa@gmail.com>
2023-03-29 10:07:55 -0300

    Added email plugin config

---
+++ src/api/email/controllers/mail.js
@@ -0,0 +1,27 @@
+const { createCoreController } = require('@strapi/strapi').factories;
+
+const emailTemplate = {
+  subject: 'Welcome Test',
+  text: `Welcome to mywebsite.fr!
+    Your account is now linked with: Test.`,
+  html: `<h1>Welcome to mywebsite.fr!</h1>
+    <p>Your account is now linked with: Test.<p>`,
+};
+
+module.exports = createCoreController('api::email.email', ({ strapi }) =>  ({
+   async sendContactMail(ctx) {
+     await strapi.plugins['email'].services.email.sendTemplatedEmail(
+       {
+         to: 'guido.glarosa@gmail.com',
+         // from: is not specified, the defaultFrom is used.
+       },
+         emailTemplate,
+       {
+         user: {
+           firstname: 'Guido',
+           email: 'guido.glarosa@gmail.com'
+         },
+       }
+     );
+   }
+}));
